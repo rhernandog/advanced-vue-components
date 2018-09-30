1078171706b1d0c119fac9098b97b0c4b2539bab
Rodrigo Hernando <rhernandog@gmail.com>
2018-09-30 01:54:27 -0300

    Date Picker completed.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <div class="min-h-screen bg-grey-darker p-8">
-    <div class="max-w-sm mx-auto">
-      <h1 class="text-2xl font-bold mb-6">Advanced Vue Components Barebone Files</h1>
+  <div class="min-h-screen bg-grey-darker p-4">
+    <div class="max-w-lg mx-auto">
+      <h1 class="text-2xl text-grey-lightest font-bold mb-6 text-center">
+        Date Picker Vue Component
+      </h1>
+
+      <div class="max-w-sm mx-auto bg-grey-lightest py-6 px-3 shadow-lg
+rounded">
+        <h4 class="mb-4">Select a Date</h4>
+        <DatePicker
+          v-model="date"
+          :options="{
+            format :'MMM DD, YYYY',
+            firstDay: 1
+          }"
+        ></DatePicker>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+// get the daypicker
+import DatePicker from "./components/DatePicker";
+// eslint-disable-next-line
+console.clear();
 
 export default {
   components: {
-    
-  }
+    DatePicker
+  },
+  
+	data(){
+    return {
+      date: "Jun 22, 2018"
+    };
+  },
 }
 </script>
 
