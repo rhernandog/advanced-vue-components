f8d810832424e67c82a4c0932c09beddd45a13ab
Rodrigo Hernando <rhernandog@gmail.com>
2018-09-30 03:31:54 -0300

    Start up commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="min-h-screen bg-grey-darker p-8">
-    <div class="max-w-sm mx-auto">
-      <h1 class="text-2xl font-bold mb-6">Advanced Vue Components Barebone Files</h1>
+  <div class="min-h-screen">
+    <div class="flex flex-wrap justify-center min-h-screen p-4">
+      
+      <div class="">
+        <h1 class="text-2xl font-bold mb-6 text-center">Modal Sample</h1>
+      </div>
+      
+      <div class="w-full text-center">
+        <button class="btn btn-blue">Show Modal</button>
+      </div>
+
     </div>
+
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -6,3 +6,8 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App)
 }).$mount("#app")
+
+/*
+  startup code: https://codesandbox.io/s/1v1o4lvp9j?from-embed
+  START UP VID: 4
+*/
