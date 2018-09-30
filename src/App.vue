3b980cf7d3468a4ce47aed5a95333f65e4ea46d5
Rodrigo Hernando <rhernandog@gmail.com>
2018-09-30 13:50:36 -0300

    Completed Sample

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,51 @@
 <template>
-  <div class="min-h-screen">
+  <div>
+    <!-- content -->
     <div class="flex flex-wrap justify-center min-h-screen p-4">
-      
+        
       <div class="">
         <h1 class="text-2xl font-bold mb-6 text-center">Modal Sample</h1>
       </div>
       
       <div class="w-full text-center">
-        <button class="btn btn-blue">Show Modal</button>
+        <button
+          @click="toggleModal"
+          class="btn btn-blue"
+        >
+          Show Modal
+        </button>
       </div>
 
     </div>
 
+    <!-- modal -->
+    <content-modal
+      :show="showModal"
+      @close="toggleModal"
+    ></content-modal>
+      
   </div>
 </template>
 
 <script>
+// get modal
+import ContentModal from "./components/SnapModal";
 
 export default {
   components: {
-    
+    ContentModal
+  },
+
+  data(){
+    return {
+      showModal: false
+    };
+  },
+
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal;
+    }
   }
 }
 </script>
