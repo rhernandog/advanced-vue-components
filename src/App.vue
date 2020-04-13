32db32881d22a648a6620cb50a49b7f44ea75ddc
Rodrigo Hernando <rhernandog@gmail.com>
2020-04-13 13:59:33 -0400

    Updated packages for security wrnings

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="min-h-screen bg-grey-darker p-8">
+    <div class="max-w-sm mx-auto">
+      <h1 class="text-2xl font-bold mb-6">Advanced Vue Components Barebone Files</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+    
+  }
+}
+</script>
+
+<style src="./assets/css/app.css"/>
